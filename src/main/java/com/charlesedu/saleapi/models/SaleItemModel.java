d532cfac4dbbc407a5af78f3abc823d32376e3cd
unknown <charleseduardofaria@gmail.com>
2024-01-11 11:05:40 -0300

    SaleItemModel e SaleItemPK (Decomposição N para N)

---
+++ src/main/java/com/charlesedu/saleapi/models/SaleItemModel.java
@@ -0,0 +1,80 @@
+package com.charlesedu.saleapi.models;
+
+import com.charlesedu.saleapi.models.keys.SaleItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "TB_SALE_ITEM")
+public class SaleItemModel {
+
+    @EmbeddedId
+    private SaleItemPK id = new SaleItemPK();
+
+    private Integer quantity;
+    private Double price;
+
+    public SaleItemModel() {
+    }
+
+    public SaleItemModel(SaleModel sale, ProductModel product, Integer quantity, Double price) {
+        this.id.setSale(sale);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    @JsonIgnore
+    public SaleModel getSale() {
+        return id.getSale();
+    }
+
+    public void setSale(SaleModel sale) {
+        this.id.setSale(sale);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SaleItemModel other = (SaleItemModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
---
+++ src/main/java/com/charlesedu/saleapi/models/keys/SaleItemPK.java
@@ -0,0 +1,68 @@
+package com.charlesedu.saleapi.models.keys;
+
+import com.charlesedu.saleapi.models.ProductModel;
+import com.charlesedu.saleapi.models.SaleModel;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@SuppressWarnings("serial")
+@Embeddable
+public class SaleItemPK {
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id")
+    private SaleModel sale;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private ProductModel product;
+
+    public SaleModel getSale() {
+        return sale;
+    }
+
+    public void setSale(SaleModel sale) {
+        this.sale = sale;
+    }
+
+    public ProductModel getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductModel product) {
+        this.product = product;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((sale == null) ? 0 : sale.hashCode());
+        result = prime * result + ((product == null) ? 0 : product.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SaleItemPK other = (SaleItemPK) obj;
+        if (sale == null) {
+            if (other.sale != null)
+                return false;
+        } else if (!sale.equals(other.sale))
+            return false;
+        if (product == null) {
+            if (other.product != null)
+                return false;
+        } else if (!product.equals(other.product))
+            return false;
+        return true;
+    }
+}
